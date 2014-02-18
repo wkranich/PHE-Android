57abee713996faefab4414a68f7c2a157534453c
William Kranich <wkranich@bu.edu>
2014-02-17 20:20:09 -0500

    added text placeholders to faq_info_page

--- PHEmobile/src/org/peerhealthexchange/phemobile/faq_info_page.java
+++ PHEmobile/src/org/peerhealthexchange/phemobile/faq_info_page.java
@@ -3,11 +3,15 @@ package org.peerhealthexchange.phemobile;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class faq_info_page extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.faq_info_page);
+		
+		TextView questionText = (TextView) findViewById(R.id.questionText);
+		TextView answerText = (TextView) findViewById(R.id.answerText);
 	}
 }
