ac06b1c71335b2430b87b9c359afc5244e6b15d2
William Kranich <wkranich@bu.edu>
2014-02-26 21:02:07 -0500

    Changed flashcards, eatingdisorders_video(lolz), messages, videos, and hotlines to fragments

--- PHEmobile/src/org/peerhealthexchange/phemobile/hotlines.java
+++ PHEmobile/src/org/peerhealthexchange/phemobile/hotlines.java
@@ -1,29 +1,23 @@
 package org.peerhealthexchange.phemobile;
 
 import android.app.ActionBar;
-import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.SearchView;
+import android.view.ViewGroup;
 
-public class hotlines extends Activity {
+public class hotlines extends Fragment {
 	
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.hotlines);
-		ActionBar locationPage = getActionBar();
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.videos, container, false);
+		ActionBar locationPage = getActivity().getActionBar();
 		locationPage.setTitle("Hotlines");
 		// Button postQuestion = (Button) findViewById(R.id.postButt);
 		// Button recentActivity = (Button) findViewById(R.id.recentButt);
-
+		return rootView;
 	}
 }
