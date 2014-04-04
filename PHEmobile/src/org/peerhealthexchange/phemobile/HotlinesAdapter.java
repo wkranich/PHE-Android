eee691cda1d0e6700966728df1062cce74a0ef6c
William Kranich <wkranich@bu.edu>
2014-04-04 18:48:35 -0400

    Added circle indicators programatically. Gotta add conditionals to know which ones to set.

--- PHEmobile/src/org/peerhealthexchange/phemobile/HotlinesAdapter.java
+++ PHEmobile/src/org/peerhealthexchange/phemobile/HotlinesAdapter.java
@@ -1,6 +1,11 @@
 package org.peerhealthexchange.phemobile;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -32,9 +37,39 @@ public class HotlinesAdapter extends ArrayAdapter<String> {
     
     textView.setText(values[position]);
     
+    BitmapFactory.Options myOptions = new BitmapFactory.Options();
+    myOptions.inDither = true;
+    myOptions.inScaled = false;
+    myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+    myOptions.inPurgeable = true;
 
-    imageView2.setImageResource(R.drawable.common_signin_btn_icon_dark);
-    imageView3.setImageResource(R.drawable.common_signin_btn_icon_dark);
+    //green color circle
+    Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+    Bitmap bmp = Bitmap.createBitmap(100, 100, conf);
+    Paint paint = new Paint();
+    paint.setAntiAlias(true);
+    paint.setColor(Color.parseColor("#3BB24D"));
+
+    Canvas canvas = new Canvas(bmp);
+    canvas.drawCircle(60, 50, 25, paint);
+
+    imageView2.setAdjustViewBounds(true);
+    imageView2.setImageBitmap(bmp);
+    
+    
+    //pink color circle
+    bmp = Bitmap.createBitmap(100, 100, conf);
+    paint.setColor(Color.parseColor("#FA3475"));
+    canvas = new Canvas(bmp);
+    canvas.drawCircle(60, 50, 25, paint);
+    imageView3.setAdjustViewBounds(true);
+    imageView3.setImageBitmap(bmp);
+    
+    //yellow hex = #FDD52B
+    
+
+    //imageView2.setImageResource(R.drawable.common_signin_btn_icon_dark);
+    //imageView3.setImageResource(R.drawable.common_signin_btn_icon_dark);
 
     return rowView;
   }
